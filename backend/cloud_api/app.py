75adb153f2e640b1a665d0f04f9ee0f8c9489ef4
newsyd04 <newsyd04@gmail.com>
2024-12-19 03:46:54 +0000

    fixing backend

--- backend/cloud_api/app.py
+++ backend/cloud_api/app.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from cloud_api.database import engine, SessionLocal
 from cloud_api.models import Device, MetricType, MetricSnapshot, MetricValue
+from datetime import datetime
 
 DATABASE_URL = "postgresql://metricsdb_yqja_user:entPz514UvRf9JX3KneevRRy2xpktl9v@dpg-ctf0alt2ng1s738fois0-a.oregon-postgres.render.com/metricsdb_yqja"
 
